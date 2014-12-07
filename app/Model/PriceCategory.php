4e9124463757d25333e2d580a6382f3d2696e2bf
Jerome Roethlisberger <jerome.roeth@gmail.com>
2014-12-07 14:26:41 +0100

    Changed priceGroups to pricecategories in order to be conform with CakePhp

--- app/Model/PriceCategory.php
+++ app/Model/PriceCategory.php
@@ -1,12 +1,10 @@
 <?php
 App::uses('AppModel', 'Model');
-
 /**
- * PriceGroup Model
+ * PriceCategory Model
  *
- * @property Price $Price
  */
-class PriceGroup extends AppModel
+class PriceCategory extends AppModel
 {
 
 	/**
@@ -92,6 +90,24 @@ class PriceGroup extends AppModel
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
+		'deleted' => array(
+			'boolean' => array(
+				'rule' => array('boolean'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+			'notEmpty' => array(
+				'rule' => array('notEmpty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
 		'created' => array(
 			'datetime' => array(
 				'rule' => array('datetime'),
@@ -132,47 +148,5 @@ class PriceGroup extends AppModel
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
-		'deleted' => array(
-			'boolean' => array(
-				'rule' => array('boolean'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-			'notEmpty' => array(
-				'rule' => array('notEmpty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
 	);
-
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
-
-	/**
-	 * hasMany associations
-	 *
-	 * @var array
-	 */
-	public $hasMany = array(
-		'Price' => array(
-			'className' => 'Price',
-			'foreignKey' => 'price_group_id',
-			'dependent' => false,
-			'conditions' => '',
-			'fields' => '',
-			'order' => '',
-			'limit' => '',
-			'offset' => '',
-			'exclusive' => '',
-			'finderQuery' => '',
-			'counterQuery' => ''
-		)
-	);
-
 }
--- app/Test/Fixture/PriceCategoryFixture.php
+++ app/Test/Fixture/PriceCategoryFixture.php
@@ -1,10 +1,9 @@
 <?php
-
 /**
- * PriceGroupFixture
+ * PriceCategoryFixture
  *
  */
-class PriceGroupFixture extends CakeTestFixture
+class PriceCategoryFixture extends CakeTestFixture
 {
 
 	/**
@@ -15,12 +14,12 @@ class PriceGroupFixture extends CakeTestFixture
 	public $fields = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
 		'title' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8mb4_general_ci', 'charset' => 'utf8mb4'),
-		'price' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8mb4_general_ci', 'charset' => 'utf8mb4'),
+		'price' => array('type' => 'decimal', 'null' => false, 'default' => '0.00', 'length' => '15,2', 'unsigned' => false),
+		'deleted' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'created_by' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'modified_by' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
-		'deleted' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1)
 		),
@@ -36,12 +35,12 @@ class PriceGroupFixture extends CakeTestFixture
 		array(
 			'id' => 1,
 			'title' => 'Lorem ipsum dolor sit amet',
-			'price' => 'Lorem ipsum dolor sit amet',
-			'created' => '2014-12-07 13:46:16',
+			'price' => '',
+			'deleted' => 1,
+			'created' => '2014-12-07 14:24:05',
 			'created_by' => 1,
-			'modified' => '2014-12-07 13:46:16',
-			'modified_by' => 1,
-			'deleted' => 1
+			'modified' => '2014-12-07 14:24:05',
+			'modified_by' => 1
 		),
 	);
 
