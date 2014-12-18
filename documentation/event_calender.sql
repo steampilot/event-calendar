d4df9d35fc3ec76a74262cd4cab54de07123b669
steampilot <github@steampilot.ch>
2014-12-18 08:11:47 +0100

    Updated SQL Dump for documentation

--- documentation/event_calender.sql
+++ documentation/event_calender.sql
@@ -1,8 +1,10 @@
--- MySQL dump 10.13  Distrib 5.6.17, for Win64 (x86_64)
+CREATE DATABASE  IF NOT EXISTS `steampil_event_calendar` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE `steampil_event_calendar`;
+-- MySQL dump 10.13  Distrib 5.5.40, for debian-linux-gnu (x86_64)
 --
--- Host: localhost    Database: event_calender
+-- Host: localhost    Database: steampil_event_calendar
 -- ------------------------------------------------------
--- Server version	5.6.14
+-- Server version	5.5.40-cll
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -27,10 +29,11 @@ CREATE TABLE `events` (
   `genre_id` int(11) NOT NULL,
   `title` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
   `lineup` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
-  `description` text CHARACTER SET utf8mb4 NOT NULL,
   `duration` datetime NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci,
   `image_title` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
-  `image_url` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `image_file_name` varchar(255) CHARACTER SET utf8mb4 DEFAULT '',
+  `deleted` tinyint(1) NOT NULL DEFAULT '0',
   `created` datetime DEFAULT NULL,
   `created_by` int(11) DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
@@ -38,7 +41,7 @@ CREATE TABLE `events` (
   PRIMARY KEY (`id`),
   KEY `fk_events_genres_idx` (`genre_id`),
   CONSTRAINT `fk_events_genres` FOREIGN KEY (`genre_id`) REFERENCES `genres` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -47,11 +50,12 @@ CREATE TABLE `events` (
 
 LOCK TABLES `events` WRITE;
 /*!40000 ALTER TABLE `events` DISABLE KEYS */;
+INSERT INTO `events` VALUES (1,3,'Januar Event 2','John, Kyra, SomeOne Else','0000-00-00 01:20:00','Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo. Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo. Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.','Some image title i would have to think of','1.jpg',0,NULL,NULL,'2014-12-14 22:05:54',1),(2,3,'Next Concert','Miles Davis and Charls P Grey','0000-00-00 00:00:00','Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo. Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo. Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.','some other image title','2.jpg',0,NULL,NULL,'2014-12-14 22:05:19',1),(3,1,'new Event','Joana Starling','0000-00-00 00:30:00','Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo. Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo. Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.','Joana Starling might not be on this record at all','3.jpg',0,NULL,NULL,NULL,NULL),(4,3,'ASDF','ASDF','0000-00-00 02:00:00','ASDFASDF','','1.jpg',1,'2014-12-15 07:17:21',1,'2014-12-15 07:17:21',NULL);
 /*!40000 ALTER TABLE `events` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `Genres`
+-- Table structure for table `genres`
 --
 
 DROP TABLE IF EXISTS `genres`;
@@ -66,17 +70,17 @@ CREATE TABLE `genres` (
   `modified` datetime DEFAULT NULL,
   `modified_by` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Genres`
+-- Dumping data for table `genres`
 --
 
 LOCK TABLES `genres` WRITE;
-/*!40000 ALTER TABLE `Genres` DISABLE KEYS */;
-INSERT INTO `genres` VALUES (1,'Rock',0,'2014-12-07 13:19:08',1,'2014-12-07 13:19:08',NULL),(2,'Blues',1,'2014-12-07 13:19:20',1,'2014-12-07 13:19:20',NULL);
-/*!40000 ALTER TABLE `Genres` ENABLE KEYS */;
+/*!40000 ALTER TABLE `genres` DISABLE KEYS */;
+INSERT INTO `genres` VALUES (1,'Rock',0,'2014-12-07 21:07:57',1,'2014-12-07 21:07:57',NULL),(2,'HipHop',0,'2014-12-07 21:08:15',1,'2014-12-07 23:53:59',1),(3,'Blues g',0,NULL,NULL,'2014-12-17 08:40:19',1),(4,'Jazz',0,'0000-00-00 00:00:00',0,'0000-00-00 00:00:00',0),(5,'Commedy',0,'0000-00-00 00:00:00',0,'0000-00-00 00:00:00',0),(6,'Classic',0,'0000-00-00 00:00:00',0,'0000-00-00 00:00:00',NULL),(7,'Neu',1,'2014-12-12 07:02:09',1,'2014-12-12 07:02:09',NULL);
+/*!40000 ALTER TABLE `genres` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -91,6 +95,7 @@ CREATE TABLE `links` (
   `event_id` int(11) NOT NULL,
   `title` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
   `link` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `deleted` tinyint(1) NOT NULL DEFAULT '0',
   `created` datetime DEFAULT NULL,
   `created_by` int(11) DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
@@ -98,7 +103,7 @@ CREATE TABLE `links` (
   PRIMARY KEY (`id`),
   KEY `fk_links_events1_idx` (`event_id`),
   CONSTRAINT `fk_links_events` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -107,6 +112,7 @@ CREATE TABLE `links` (
 
 LOCK TABLES `links` WRITE;
 /*!40000 ALTER TABLE `links` DISABLE KEYS */;
+INSERT INTO `links` VALUES (1,1,'Event Infos','http://www.steampilot.ch/demo/spcb',0,'2014-12-14 22:04:07',1,'2014-12-14 22:04:07',NULL),(2,1,'Some more Informations','http://www.steampilot.ch',0,'2014-12-14 22:04:36',1,'2014-12-14 22:04:36',NULL);
 /*!40000 ALTER TABLE `links` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -119,15 +125,14 @@ DROP TABLE IF EXISTS `price_categories`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `price_categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
-  `price` decimal(15,2) NOT NULL DEFAULT '0.00',
+  `title` varchar(255) NOT NULL,
   `deleted` tinyint(1) NOT NULL DEFAULT '0',
   `created` datetime DEFAULT NULL,
   `created_by` int(11) DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   `modified_by` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -136,7 +141,7 @@ CREATE TABLE `price_categories` (
 
 LOCK TABLES `price_categories` WRITE;
 /*!40000 ALTER TABLE `price_categories` DISABLE KEYS */;
-INSERT INTO `price_categories` VALUES (1,'Front Row',12.45,0,NULL,NULL,NULL,NULL),(2,'Left Tribune',25.25,0,NULL,NULL,NULL,NULL),(3,'Seated Back Row',100.00,1,NULL,NULL,NULL,NULL),(4,'Backstage Ticket',450.15,0,NULL,NULL,'2014-12-07 16:25:38',1),(5,'Guest Sector Alpha 3',45.00,0,'2014-12-07 16:32:00',1,'2014-12-07 16:32:00',NULL);
+INSERT INTO `price_categories` VALUES (1,'Front Row',0,'0000-00-00 00:00:00',0,'0000-00-00 00:00:00',0),(2,'Middle Row',0,'0000-00-00 00:00:00',0,'0000-00-00 00:00:00',0),(3,'Balcony',0,'0000-00-00 00:00:00',0,'2014-12-12 14:59:58',1),(4,'CulturePass',0,'0000-00-00 00:00:00',0,'0000-00-00 00:00:00',0),(5,'Famaly Ticket',0,NULL,NULL,NULL,NULL),(6,'Neue Preiskategorie',0,'2014-12-12 07:00:59',1,'2014-12-12 07:00:59',NULL),(7,'hgINZTERSATE REIHE',0,'2014-12-15 07:15:19',1,'2014-12-15 07:15:19',NULL);
 /*!40000 ALTER TABLE `price_categories` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -150,17 +155,19 @@ DROP TABLE IF EXISTS `prices`;
 CREATE TABLE `prices` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `event_id` int(11) NOT NULL,
-  `price_group_id` int(11) NOT NULL,
+  `price_category_id` int(11) NOT NULL,
+  `price` decimal(15,2) NOT NULL,
+  `deleted` tinyint(1) NOT NULL DEFAULT '0',
   `created` datetime DEFAULT NULL,
   `created_by` int(11) DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   `modified_by` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_Prices_events1_idx` (`event_id`),
-  KEY `fk_Prices_price_groups1_idx` (`price_group_id`),
+  KEY `fk_Prices_price_groups1_idx` (`price_category_id`),
   CONSTRAINT `fk_prices_events` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  CONSTRAINT `fk_prices_price_groups` FOREIGN KEY (`price_group_id`) REFERENCES `price_categories` (`id`) ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  CONSTRAINT `fk_prices_price_groups` FOREIGN KEY (`price_category_id`) REFERENCES `price_categories` (`id`) ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -169,6 +176,7 @@ CREATE TABLE `prices` (
 
 LOCK TABLES `prices` WRITE;
 /*!40000 ALTER TABLE `prices` DISABLE KEYS */;
+INSERT INTO `prices` VALUES (12,1,2,23.40,0,'2014-12-14 22:02:31',1,'2014-12-14 22:02:31',NULL),(13,1,5,234.00,0,'2014-12-14 22:02:41',1,'2014-12-14 22:02:41',NULL),(14,1,5,56.20,0,'2014-12-14 22:03:11',1,'2014-12-14 22:03:11',NULL),(15,4,7,50.00,0,'2014-12-15 07:17:51',1,'2014-12-15 07:17:51',NULL);
 /*!40000 ALTER TABLE `prices` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -183,6 +191,7 @@ CREATE TABLE `shows` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `event_id` int(11) NOT NULL,
   `begin` datetime NOT NULL,
+  `deleted` tinyint(1) NOT NULL DEFAULT '0',
   `created` datetime DEFAULT NULL,
   `created_by` int(11) DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
@@ -190,7 +199,7 @@ CREATE TABLE `shows` (
   PRIMARY KEY (`id`),
   KEY `fk_shows_events1_idx` (`event_id`),
   CONSTRAINT `fk_shows_events` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -199,6 +208,7 @@ CREATE TABLE `shows` (
 
 LOCK TABLES `shows` WRITE;
 /*!40000 ALTER TABLE `shows` DISABLE KEYS */;
+INSERT INTO `shows` VALUES (1,1,'2015-01-01 20:00:00',0,'0000-00-00 00:00:00',0,'0000-00-00 00:00:00',0),(2,1,'2015-01-02 20:15:00',0,'0000-00-00 00:00:00',0,'0000-00-00 00:00:00',0),(3,1,'2015-01-05 10:24:00',0,'0000-00-00 00:00:00',0,'0000-00-00 00:00:00',0),(4,2,'2015-03-12 16:45:00',0,'0000-00-00 00:00:00',0,'0000-00-00 00:00:00',0),(5,2,'2015-11-24 12:23:00',0,NULL,NULL,NULL,NULL);
 /*!40000 ALTER TABLE `shows` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -214,6 +224,7 @@ CREATE TABLE `users` (
   `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `username` varchar(45) CHARACTER SET utf8mb4 NOT NULL,
   `password` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `deleted` tinyint(1) NOT NULL DEFAULT '0',
   `created` datetime DEFAULT NULL,
   `created_by` int(11) DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
@@ -229,7 +240,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Admin','info@steampilot.ch','$2y$10$.K45qikSIM8s47a7E4PZQ.pRXvpEYIlEX.nJyinANqRVsRZFRxS3G',NULL,NULL,NULL,NULL);
+INSERT INTO `users` VALUES (1,'Admin','info@steampilot.ch','$2y$10$.K45qikSIM8s47a7E4PZQ.pRXvpEYIlEX.nJyinANqRVsRZFRxS3G',0,NULL,NULL,NULL,NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -242,4 +253,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-12-07 17:12:14
+-- Dump completed on 2014-12-18  8:01:19
